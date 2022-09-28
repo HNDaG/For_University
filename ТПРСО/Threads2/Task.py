d9b926f5b1a7ea28a0f61325a7e4282c1e014a02
Nikita Hohulia <nikitahohulia@gmail.com>
2022-09-28 13:49:12 +0300

    Minor changes

--- ТПРСО/Threads2/Task.py
+++ ТПРСО/Threads2/Task.py
@@ -3,77 +3,103 @@ import random as r
 import time
 
 
-class Rise:
-    arr = []
+class Rice:
+    arr = list()
     def __init__(self, value = 0.0):
         self._mass = value
 
     @property
     def mass(self):
-        return self._mass
-
-    @mass.setter
-    def mass(self, value):
-        self._mass = value
-
-class Potatoes:
-    arr = []
-    def __init__(self, value = 0.0):
-        self._mass = value
-
-    @property
-    def mass(self):
-        return self._mass
-
-    @mass.setter
-    def mass(self, value):
-        self._mass = value
-
-
-def adder(cl, value):
-    for i in range(value):
-        to_add = r.random()
         try:
             lock = Lock()
             lock.acquire()
-            cl.arr.append(cl.mass)
-            cl.mass += to_add
+            value = self._mass 
+        finally:
+            
+            lock.release()
+            return value
+    def set_mass(self, value):
+        try:
+            lock = Lock()
+            lock.acquire()
+            self._mass += value
+            self.arr.append(self.mass)
+        finally:
+            lock.release()    
+    
+
+class Potatoes:
+    arr = list()
+    def __init__(self, value = 0.0):
+        self._mass = value
+
+    @property
+    def mass(self):
+        try:
+            lock = Lock()
+            lock.acquire()
+            value = self._mass 
+        finally:
+
+            lock.release()
+            return value
+
+  
+    def set_mass(self, value):
+        try:
+            lock = Lock()
+            lock.acquire()
+            self._mass += value
+            self.arr.append(self.mass)
         finally:
             lock.release()
 
-
+    
+def adder(cl, value):
+    for i in range(value):
+        to_add = r.random()
+        cl.set_mass(to_add)
+            
+    
 if __name__ == '__main__':
-    R = Rise()
+    R = Rice()
     P = Potatoes()
+
+    le = 5000
+    ri = 10000
+
+    rnd1 = r.randint(le, ri)
+    rnd2 = r.randint(le, ri)
 
     start = time.time()
     threads = []
     N = r.randint(10, 30)
     for n in range(N):
         if n < N / 2:
-            t = Thread(target=adder, args=(P, r.randint(10000, 20000)))
+            t = Thread(target=adder, args=(P, rnd1))
             threads.append(t)
             threads[-1].start()
         else:
-            t = Thread(target=adder, args=(R, r.randint(10000, 20000)))
+            t = Thread(target=adder, args=(R, rnd2))
             threads.append(t)
             threads[-1].start()
 
     for thread in threads:
         thread.join()
 
-
     print(f"Answer is {(R.mass, P.mass)}")
     print(f"Time is {time.time() - start}")
+
+    
     # check is it accurate
-    '''
+    
     for i in range(len(R.arr)-1):
         if R.arr[i] in R.arr[i+1:]:
             print("error")
-
+    print("Rice is done...")
     for i in range(len(P.arr)-1):
         if P.arr[i] in P.arr[i+1:]:
             print("error")
-    print("Ok")
-    '''
+    print("Potatoes is done...")
+    
 
