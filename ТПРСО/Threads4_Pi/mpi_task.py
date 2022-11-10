de5c0a8e0566d5357a43516263ceb7c0a38955e3
NikitaHohulia <nikitahohulia@gmail.com>
2022-11-10 12:11:52 +0200

    Minor changes

--- ТПРСО/Threads4_Pi/mpi_task.py
+++ ТПРСО/Threads4_Pi/mpi_task.py
@@ -1,9 +1,17 @@
 from mpi4py import MPI
 import random as r
-import time
+import sys
+import argparse
 
 PI = 3.141592653589793
-points = 100000
+
+points = 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("points", help="display the total number of points",type=int)
+args = parser.parse_args()
+points = int(sys.argv[1])
+
 
 def In_zone(x, y):
     return (x*x+y*y)<1
@@ -29,7 +37,22 @@ def main():
     pi = 4.0 * in_zone / points_to_calculate;
     end = MPI.Wtime()
 
-    print(f"Rank: {rank}\tPoints_to_calculate: {points_to_calculate}\nTime: {end-start}\tpi: {pi}\t Error: {abs(PI-pi)}" )
+
+    ans = dict()
+    if rank != 0:
+        data = [rank, points_to_calculate, end-start, pi, abs(PI-pi)]
+        comm.send(data, dest=0)
+    
+    else:
+        ans[rank] = [points_to_calculate, end-start, pi, abs(PI-pi)]
+        for k in range(1, size):
+            val = comm.recv(source=k)
+            ans[val[0]] = val[1:]
+
+    if rank == 0:
+        for j in range(1, size):
+            print(f"Rank: {j}\tPoints_to_calculate: {ans[j][0]}\nTime: {ans[j][1]}\tpi: {ans[j][2]}\t Error: {ans[j][3]}\n\n" )
+        print(f"Rank: {0}\tPoints_to_calculate: {ans[0][0]}\nTime: {ans[0][1]}\tpi: {ans[0][2]}\t Error: {ans[0][3]}\n\n" )
 
 main()
 
